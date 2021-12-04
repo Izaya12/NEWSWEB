4704e6b95f1e46c675f2d117bfacf8475632dd47
Fern Kamui <20140005811@my.xu.edu.ph>
2021-12-04 14:12:04 +0800

    first commit

---
+++ resources/views/promotional.blade.php
@@ -0,0 +1,173 @@
+<!doctype html>
+<html lang="en">
+  <head>
+   
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+   
+    <link rel="stylesheet" type="text/css" href="{{URL('/css/index.css')}}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    
+  
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">CDU NEWS</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+    <li ><a style="background-color:#AFEEEE" class="nav-link active" aria-current="page" href="/index">Home</a></li> 
+    <li><a style="background-color: #AFEEEE" class="nav-link" href="/tabloid">Tabloid</a></li>
+    <li><a style="background-color: #AFEEEE" class="nav-link" href="/government">Government</a></li>
+    <li><a style="background-color: #AFEEEE" class="nav-link" href="/sports">Sports</a></li>
+    <li><a style="background-color: #AFEEEE" class="nav-link" href="/entertainment">Entertainment</a></li>
+    <li><a style="background-color: #AFEEEE" class="nav-link" href="/getlotto">Get Lotto</a></li>
+    <li><a style="background-color: #AFEEEE" class="nav-link" href="/recommended">Recommended</a></li>
+    <li><a style="background-color: #AFEEEE" class="nav-link" href="/promotional">Promotional</a></li>
+    <li><a style="background-color: #AFEEEE" class="nav-link" href="/ads">Ads</a></li>
+    </div>
+  </div>
+</nav>
+
+<body style="background-color: #D3D3D3" ></body>
+
+
+
+@foreach ($data['result'] as $dat)
+</br>
+<div class="container">
+<div class="card mb-3" style="max-width: 1400px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="{{ $dat['image_url']}}" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">{{ $dat['title']}}</h5>
+        <p class="card-text">{{ $dat['category']}}</p>
+        <p class="card-text"><small class="text-muted"><?php echo $data['date'] ?></small></p>
+        <p class="card-text"><small class="text-muted">Publish at:{{ $dat['publish_date']}}</small></p>
+        <a style="background-color:#DE3163" href="{{ $dat['link']}}" class ="btn bg-dark: text-white">READ MORE</a>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+@endforeach
+
+
+  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+  
+  </body>
+</html>
+
+
+<footer
+          class="text-center text-lg-start text-white"
+          style="background-color: #1c2331"
+          >
+  
+    <section
+             class="d-flex justify-content-between p-4"
+             style="background-color: #6351ce"
+             >
+    
+     
+    </section>
+    
+    <section class="">
+      <div class="container text-center text-md-start mt-5">
+      
+        <div class="row mt-3">
+        
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+       
+            <h6 class="text-uppercase fw-bold">Cosmic Digital Universe</h6>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 60px; background-color: #7c4dff; height: 2px"
+                />
+            <p>
+            CDU is a leading content and digital service provider in the Philippines. We offer comprehensive mobile solutions that cover mobile content sourcing and management, application development and porting as well as mobile billing. CDU is composed of young and dynamic individuals who share the same passion of providing users with more localized flavor of products and services.
+            </p>
+          </div>
+        
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+        
+            <h6 class="text-uppercase fw-bold">Products</h6>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 60px; background-color: #7c4dff; height: 2px"
+                />
+            <p>
+              <a href="#!" class="text-white">Chums Live</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">FLAMES</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">Info Center</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">Fun Club</a>
+            </p>
+          </div>
+       
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+           
+            <h6 class="text-uppercase fw-bold">Useful links</h6>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 60px; background-color: #7c4dff; height: 2px"
+                />
+            <p>
+              <a href="#!" class="text-white">Your Account</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">Become an Affiliate</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">Shipping Rates</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">Help</a>
+            </p>
+          </div>
+        
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            
+            <h6 class="text-uppercase fw-bold">Contact</h6>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 60px; background-color: #7c4dff; height: 2px"
+                />
+            <p><i class="fas fa-home mr-3"></i> 1360 Leon Guinto Sr. St. Ermita, Manila</p>
+            <p><i class="fas fa-envelope mr-3"></i> Email Address:info@cdu.com.ph</p>
+            <p><i class="fas fa-phone mr-3"></i> Number:+63 (2) 8353 - 0170</p>
+          </div>
+      
+        </div>
+    
+      </div>
+    </section>
+  
+    <div
+         class="text-center p-3"
+         style="background-color: rgba(0, 0, 0, 0.2)"
+         >
+      © 2021 Copyright:
+      <a class="text-white" href="https://mdbootstrap.com/"
+         >COSMIC DIGITAL UNIVERSE</a
+        >
+    </div>
+    
+  </footer>
+
+
+</div>
+
